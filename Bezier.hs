d72a9d239783bc123bf6abdb7b0a827c337080ba
harold <harold@mit.edu>
2011-12-03 15:43:15 -0500

    clarified like butter

--- Bezier.hs
+++ Bezier.hs
@@ -1,16 +1,16 @@
 module Bezier (bezier) where
 
--- linear interpolation between two points of arbitrary dimension:
-interpolate a b t =
-  map interpolate' (zip a b)
-  where interpolate' (a', b') = (1 - t)*a' + t*b'
+-- parametric line between two points of arbitrary dimension:
+line :: Num a => [a] -> [a] -> a -> [a]
+line p q t = map interpolate (zip p q)
+  where interpolate (a, b) = (1 - t)*a + t*b
 
--- bezier curve of just one point is fixed at that point:
-bezier [point] t = point
--- bezier curve of a list of points is just linear interpolation
--- between bezier curve of the initial part of the list and bezier
--- curve of the tail of the list:
-bezier points  t =
-  interpolate (bezier (init points) t)
-              (bezier (tail points) t)
-              t
+-- bezier of just one point is fixed at that point,
+-- and bezier of a list of points is just linear interpolation between
+-- bezier of the initial part of the list and bezier of the tail of
+-- the list:
+bezier :: Num a => [[a]] -> a -> [a]
+bezier [p] t = p
+bezier ps  t = line (bezier (init ps) t)
+                    (bezier (tail ps) t)
+                    t
