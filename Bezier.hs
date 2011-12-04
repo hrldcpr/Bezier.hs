4f49d51a350469c12345d7cd9a8b54c44077bbf0
hrldcpr <hrldcpr@gmail.com>
2011-12-04 16:33:04 -0500

    Point type for clarity

--- Bezier.hs
+++ Bezier.hs
@@ -1,7 +1,9 @@
 module Bezier (bezier) where
 
+type Point = []
+
 -- parametric line between two points of arbitrary dimension:
-line :: Num a => [a] -> [a] -> a -> [a]
+line :: Num a => Point a -> Point a -> a -> Point a
 line p q t = map interpolate (zip p q)
   where interpolate (a, b) = (1 - t)*a + t*b
 
@@ -9,7 +11,7 @@ line p q t = map interpolate (zip p q)
 -- and bezier of a list of points is just linear interpolation between
 -- bezier of the initial part of the list and bezier of the tail of
 -- the list:
-bezier :: Num a => [[a]] -> a -> [a]
+bezier :: Num a => [Point a] -> a -> Point a
 bezier [p] t = p
 bezier ps  t = line (bezier (init ps) t)
                     (bezier (tail ps) t)
