b51d2629f0d0b75bd98faac071c70c3ac4034cd5
harold cooper <hrldcpr@gmail.com>
2012-12-07 21:17:50 -0500

    semantic line breaks

--- Bezier.hs
+++ Bezier.hs
@@ -9,9 +9,9 @@ line p q t = zipWith interpolate p q
   where interpolate a b = (1 - t)*a + t*b
 
 -- bezier of just one point is fixed at that point,
--- and bezier of a list of points is just linear interpolation between
--- bezier of the initial part of the list and bezier of the tail of
--- the list:
+-- and bezier of a list of points is just linear interpolation
+-- between bezier of the initial part of the list
+-- and bezier of the tail of the list:
 bezier :: Num a => [Point a] -> a -> Point a
 bezier [p] t = p
 bezier ps  t = line (bezier (init ps) t)
