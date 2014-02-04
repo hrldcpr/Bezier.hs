0b4b0402b84f96e92b0db4f81f5e0173a12f0eb7
Harold Cooper <hcooper@ebay.com>
2014-02-04 18:24:57 -0500

    time display, ability to pause, and always draw progress points

--- Main.hs
+++ Main.hs
@@ -1,4 +1,6 @@
 
+import Text.Printf
+
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Bezier
@@ -7,21 +9,24 @@ bezier2d ps t =
   let [x, y] = bezier [[x, y] | (x, y) <- ps] t
   in (x, y)
 
-data World = World {points :: [Point], time :: Float, depth :: Int, verbose :: Bool}
+data World = World {points :: [Point], time :: Float, depth :: Int, verbose :: Bool, animating :: Bool}
 
-initialWorld = World [(-400, -400), (-400, 400)] 0 0 False
+initialWorld = World [(-400, -400)] 0 1 False True
 
 main =
   play (InWindow "Bézier" (1000, 1000) (0,  0))
        black 50 initialWorld
        picture touched next
 
-next dt (World ps t d v) = World ps (t + dt) d v
+next dt (World ps t d v True) = World ps (t + dt) d v True
+next _ world = world
 
-touched (EventKey (MouseButton LeftButton) Down _ p) (World ps t d v) = World (ps ++ [p]) t d v
-touched (EventKey (SpecialKey KeyUp) Down _ _) (World ps t d v) = World ps t (d + 1) v
-touched (EventKey (SpecialKey KeyDown) Down _ _) (World ps t d v) = World ps t (d - 1) v
-touched (EventKey (SpecialKey KeySpace) Down _ _) (World ps t d v) = World ps t d (not v)
+touched (EventKey (MouseButton LeftButton) Down _ p) (World ps t d v a) = World (ps ++ [p]) t d v a
+touched (EventKey (SpecialKey KeyUp) Down _ _) (World ps t d v a) = World ps t (d + 1) v a
+touched (EventKey (SpecialKey KeyDown) Down _ _) (World ps t d v a) = World ps t (d - 1) v a
+touched (EventKey (Char 'v') Down _ _) (World ps t d v a) = World ps t d (not v) a
+touched (EventKey (SpecialKey KeySpace) Down _ _) (World ps t d v a) = World ps t d v (not a)
+touched (EventKey (SpecialKey KeyDelete) Down _ _) (World ps t d v a) = World (init ps) t d v a
 touched _ world = world
 
 dim' c = let
@@ -30,10 +35,11 @@ dim' c = let
   in
      makeColor (r / k) (g / k) (b / k) a
 
-picture (World ps t d v) = Pictures [
+picture (World ps t d v a) = Pictures [
       go blue green d ps,
       Color white $ Line $ map c ts,
-      Color white $ Pictures $ [Translate x y $ ThickCircle w r | (x, y) <- ps]
+      Color white $ Pictures $ [Translate x y $ ThickCircle w r | (x, y) <- ps],
+      Color green $ Translate 0 300 $ Scale 0.3 0.3 $ Text $ printf "t=%.1f" t'
     ]
   where
     w = 2
@@ -43,8 +49,8 @@ picture (World ps t d v) = Pictures [
     t' = (1 + cos t) / 2
     go _ _ 0 _ = Blank
     go _ _ _ [_] = Blank
-    go white blue d ps = let
-        go' = go (dim' white) (dim' blue) (d - 1)
+    go curveColor lineColor d ps = let
+        go' = go (dim' curveColor) (dim' lineColor) (d - 1)
         a = bezier2d (init ps)
         b = bezier2d (tail ps)
         (ax, ay) = a t'
@@ -52,8 +58,10 @@ picture (World ps t d v) = Pictures [
       in Pictures [
           go' (init ps),
           go' (tail ps),
-          if (v || d == 1) then (Color white $ Line $ map a ts) else Blank,
-          if (v || d == 1) then (Color white $ Line $ map b ts) else Blank,
-          Color blue $ Line [a t', b t'],
-          Color blue $ Translate ((1 - t')*ax + t'*bx) ((1 - t')*ay + t'*by) $ ThickCircle w r
+          Color lineColor $ Line [a t', b t'],
+          if (v || d == 1 || length ps <= 3) then (Color curveColor $ Line $ map a ts) else Blank,
+          if (v || d == 1 || length ps <= 3) then (Color curveColor $ Line $ map b ts) else Blank,
+          Color lineColor $ Translate ax ay $ ThickCircle w r,
+          Color lineColor $ Translate bx by $ ThickCircle w r,
+          Color lineColor $ Translate ((1 - t')*ax + t'*bx) ((1 - t')*ay + t'*by) $ ThickCircle w r
         ]
