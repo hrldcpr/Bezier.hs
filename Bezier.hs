296b489442884214a1fa9409f7a232756ede82c8
Harold Cooper <hrldcpr@gmail.com>
2015-02-05 17:26:16 -0500

    even more point free with applicative functors!

--- Bezier.hs
+++ Bezier.hs
@@ -1,16 +1,17 @@
 module Bezier (bezier) where
 
+import Control.Applicative
+
 type Point = [Float]
 type Curve = Float -> Point
 
--- bezier of one point is fixed at that point, and bezier of N points is interpolation
--- between bezier of first N-1 points and bezier of last N-1 points:
+-- bezier of one point is fixed at that point, and bezier of N points is the linear
+-- interpolation between bezier of first N-1 points and bezier of last N-1 points:
 bezier :: [Point] -> Curve
 bezier [p] = const p
-bezier ps  = interpolation (bezier $ init ps) (bezier $ tail ps)
+bezier ps  = linterpolate <*> (bezier $ init ps) <*> (bezier $ tail ps)
 
--- linear interpolation between two curves:
--- TODO make this point-free, a la http://stackoverflow.com/questions/4333864/trick-for-reusing-arguments-in-haskell
-interpolation :: Curve -> Curve -> Curve
-interpolation f g t = zipWith go (f t) (g t)
+-- linear interpolation between two points:
+linterpolate :: Float -> Point -> Point -> Point
+linterpolate t p q = zipWith go p q
   where go a b = (1 - t)*a + t*b
