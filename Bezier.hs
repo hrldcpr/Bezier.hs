e42551e8dfb3f19d56bdc5d2092e2a252df282f8
Harold Cooper <hrldcpr@gmail.com>
2015-02-05 19:27:07 -0500

    tidy?

--- Bezier.hs
+++ Bezier.hs
@@ -2,16 +2,16 @@ module Bezier (bezier) where
 
 import Control.Applicative
 
+type Time = Float
 type Point = [Float]
-type Curve = Float -> Point
 
 -- bezier of one point is fixed at that point, and bezier of N points is the linear
 -- interpolation between bezier of first N-1 points and bezier of last N-1 points:
-bezier :: [Point] -> Curve
+bezier :: [Point] -> Time -> Point
 bezier [p] = const p
-bezier ps  = linterpolate <*> (bezier $ init ps) <*> (bezier $ tail ps)
+bezier ps  = interpolate <*> (bezier $ init ps) <*> (bezier $ tail ps)
 
 -- linear interpolation between two points:
-linterpolate :: Float -> Point -> Point -> Point
-linterpolate t p q = zipWith go p q
+interpolate :: Time -> Point -> Point -> Point
+interpolate t p q = zipWith go p q
   where go a b = (1 - t)*a + t*b
