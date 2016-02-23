71c8c8c20c75131675361c6f4762d915eef28578
Harold Cooper <hrldcpr@gmail.com>
2016-02-23 17:41:38 -0500

    reuse sub-curves, much more efficient

--- bezier.js
+++ bezier.js
@@ -16,56 +16,53 @@
     };
   }
 
-  function bezier(ps, offset, length) {
-    if (offset === undefined) offset = 0;
-    if (length === undefined) length = ps.length;
-
-    if (length === 1) return constant(ps[offset]);
-    else return linear(bezier(ps, offset, length - 1),
-                       bezier(ps, offset + 1, length - 1));
-  }
-
-  var points = [[200, 200], [100, 500], [500, 500], [500, 100]];
-
+  var DT = 0.01;
 
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
 
-  function draw(time, offset, length) {
+  var points;
+  var time;
+  function draw(offset, length) {
     if (offset === undefined) offset = 0;
     if (length === undefined) length = points.length;
 
-    if (length === 1) return;
+    if (length === 1) return constant(points[offset]);
 
-    var p = draw(time, offset, length - 1);
-    var q = draw(time, offset + 1, length - 1);
+    var a = draw(offset, length - 1);
+    var b = draw(offset + 1, length - 1);
     if (length > 2) {  // don't draw line if bezier already is one
+      var p = a(time);
+      var q = b(time);
       ctx.beginPath();
       ctx.moveTo(p[0], p[1]);
       ctx.lineTo(q[0], q[1]);
       ctx.stroke();
     }
 
-    var curve = bezier(points, offset, length);
+    var c = linear(a, b);
     ctx.beginPath();
-    for (var t = 0; t < 1; t += 0.01) {
-      var p = curve(t);
+    for (var t = 0; t < 1 + DT; t += DT) {
+      var p = c(t);
       ctx.lineTo(p[0], p[1]);
     }
     ctx.stroke();
 
-    var l = curve(time);
+    var l = c(time);
     ctx.beginPath();
     ctx.arc(l[0], l[1], 5, 0, 2 * Math.PI);
     ctx.fill();
-    return l;
+    return c;
   }
 
-  function animate(time) {
+  function animate(t) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    draw((1 + Math.cos(time / 1000)) / 2);
+    time = (1 + Math.cos(t / 1000)) / 2;
+    draw();
     window.requestAnimationFrame(animate);
   }
+
+  points = [[200, 200], [100, 500], [500, 500], [500, 100]];
   window.requestAnimationFrame(animate);
 
 })(this, this.document);
