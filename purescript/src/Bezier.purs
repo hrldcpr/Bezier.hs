5315ef52981637170884b7f67e6c720a5215e7ff
Harold Cooper <hrldcpr@gmail.com>
2016-07-27 13:31:07 -0400

    use floats in purescript

--- purescript/src/Bezier.purs
+++ purescript/src/Bezier.purs
@@ -8,12 +8,12 @@ import Data.NonEmpty (NonEmpty, head, tail)
 import NonEmpty (forArray, zipWithA, fromArray)
 
 
-type Point = Array Int  -- a multi-dimensional coordinate
-type Parametric a = Int -> a  -- a value that varies over time
+type Point = Array Number  -- a multi-dimensional coordinate
+type Parametric a = Number -> a  -- a value that varies over time
 
 -- linear interpolation between two numbers, from t=0 to t=1
-line1d :: Int -> Int -> Parametric Int
-line1d a b = \t -> (1 - t)*a + t*b
+line1d :: Number -> Number -> Parametric Number
+line1d a b = \t -> (1.0 - t)*a + t*b
 
 -- line between two points is linear interpolation on each dimension
 line :: Point -> Point -> Parametric Point
