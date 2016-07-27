63f183c7207a9ad9e0d3d313787386e477ccf83d
Harold Cooper <hrldcpr@gmail.com>
2016-07-27 13:28:27 -0400

    purescript implementation

---
+++ purescript/src/Bezier.purs
@@ -0,0 +1,31 @@
+module Bezier where
+
+import Prelude
+import Data.Array (zipWithA) as Array
+import Data.Maybe (Maybe(..))
+import Data.NonEmpty (NonEmpty, head, tail)
+
+import NonEmpty (forArray, zipWithA, fromArray)
+
+
+type Point = Array Int  -- a multi-dimensional coordinate
+type Parametric a = Int -> a  -- a value that varies over time
+
+-- linear interpolation between two numbers, from t=0 to t=1
+line1d :: Int -> Int -> Parametric Int
+line1d a b = \t -> (1 - t)*a + t*b
+
+-- line between two points is linear interpolation on each dimension
+line :: Point -> Point -> Parametric Point
+line p q = Array.zipWithA line1d p q
+
+-- bezier of one point is fixed at that point, and bezier of n points is the
+-- line between bezier of first n-1 points and bezier of last n-1 points
+-- TODO use non-empty list of points? and use PatternSynonyms to match on either Cons a [a] or Cons' a (Maybe (Cons a))
+bezier :: NonEmpty Array Point -> Parametric Point
+bezier ps = case fromArray (tail ps) of
+  Nothing -> pure (head ps)
+  Just tail -> bezier =<< zipWithA line ps tail
+
+maybezier :: Array Point -> Maybe (Parametric Point)
+maybezier = forArray bezier
---
+++ purescript/src/NonEmpty.purs
@@ -0,0 +1,23 @@
+module NonEmpty where
+
+import Prelude
+import Data.Array (uncons)
+import Data.Array (zipWith) as Array
+import Data.Maybe (Maybe(..))
+import Data.NonEmpty (NonEmpty, (:|))
+import Data.Traversable (sequence)
+
+
+fromArray :: forall a. Array a -> Maybe (NonEmpty Array a)
+fromArray xs = case uncons xs of
+  Nothing -> Nothing
+  Just {head: x, tail: xs} -> Just (x :| xs)
+
+forArray :: forall a b. (NonEmpty Array a -> b) -> Array a -> Maybe b
+forArray f = fromArray >>> map f
+
+zipWith :: forall a b c. (a -> b -> c) -> NonEmpty Array a -> NonEmpty Array b -> NonEmpty Array c
+zipWith f (x:|xs) (y:|ys) = f x y :| Array.zipWith f xs ys
+
+zipWithA :: forall f a b c. Applicative f => (a -> b -> f c) -> NonEmpty Array a -> NonEmpty Array b -> f (NonEmpty Array c)
+zipWithA f xs ys = sequence $ zipWith f xs ys
