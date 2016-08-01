4aa6a6f079e93bffdfba0c328ee65066b3ba5818
Harold Cooper <hrldcpr@gmail.com>
2016-07-31 22:26:16 -0400

    draw both quadratic and exponential bezier constructions

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -4,10 +4,11 @@ import Prelude
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
 import Data.Array (range)
-import Data.Foldable (class Foldable, for_)
+import Data.Foldable (class Foldable, foldMap, for_)
 import Data.Int (toNumber)
 import Data.Maybe (Maybe, maybe)
-import Data.NonEmpty (NonEmpty, (:|))
+import Data.Monoid.Additive (Additive(..), runAdditive)
+import Data.NonEmpty (NonEmpty, head, (:|))
 import Graphics.Canvas (CANVAS, CanvasElement, Context2D,
                         arc, clearRect, fillPath, getCanvasDimensions, getCanvasElementById,
                         getContext2D, lineTo, moveTo, setFillStyle, setStrokeStyle, strokePath)
@@ -15,7 +16,7 @@ import Math (cos, pi)
 
 import Animation (ANIMATION, Milliseconds, requestAnimationFrame)
 import Bezier (Point, bezier, line2d)
-import Data.NonEmpty.Array (tail', zipWithA)
+import Data.NonEmpty.Array (init, tail', zipWithA)
 
 
 doMaybe :: forall eff a b. (a -> Eff eff b) -> Maybe a -> Eff eff Unit
@@ -24,11 +25,13 @@ doMaybe f = maybe (pure unit) (f >>> void)
 maybeDo :: forall eff a b. Maybe a -> (a -> Eff eff b) -> Eff eff Unit
 maybeDo = flip doMaybe
 
-points :: NonEmpty Array Point
-points = { x: 100.0, y: 500.0 } :| [{ x: 100.0, y: 100.0}, { x: 300.0, y: 100.0 }, { x: 500.0, y: 300.0}, { x: 400.0, y: 500.0 }]
+length :: forall f a. Foldable f => f a -> Int
+length = (foldMap $ const $ Additive 1) >>> runAdditive
+
 
 steps :: Int -> Array Number
 steps n = (\i -> toNumber i / toNumber n) <$> range 0 n
+
 
 circle :: forall eff. Context2D -> { x :: Number, y :: Number, r :: Number } -> Eff (canvas :: CANVAS | eff) Unit
 circle ctx { x, y, r } = void $ arc ctx { x, y, r, start: 0.0, end: 2.0 * pi }
@@ -37,6 +40,10 @@ line :: forall eff f. Foldable f => Context2D -> f Point -> Eff (canvas :: CANVA
 line ctx ps = for_ ps \p -> do
   lineTo ctx p.x p.y
   moveTo ctx p.x p.y
+
+
+points :: NonEmpty Array Point
+points = { x: 100.0, y: 500.0 } :| [{ x: 100.0, y: 100.0}, { x: 300.0, y: 100.0 }, { x: 500.0, y: 300.0}, { x: 400.0, y: 500.0 }]
 
 draw :: CanvasElement -> Milliseconds
         -> Eff (animation :: ANIMATION, canvas :: CANVAS, console :: CONSOLE) Unit
@@ -47,7 +54,8 @@ draw canvas time = void do
   clearRect ctx { x: 0.0, y: 0.0, w: width, h: height }
 
   let t = (1.0 + cos (time / 1000.0)) / 2.0
-      go ps = void $ do
+
+      go ps = do
         setStrokeStyle "orange" ctx
         strokePath ctx $ line ctx ps
 
@@ -57,8 +65,29 @@ draw canvas time = void do
         setFillStyle "black" ctx
         for_ ps \p -> fillPath ctx $ circle ctx { x: p.x, y: p.y, r: 3.0 }
 
-        maybeDo (tail' ps) \tl -> go $ zipWithA line2d ps tl $ t
-  go points
+        maybeDo (tail' ps) \ps_tail -> go $ zipWithA line2d ps ps_tail $ t
+
+      go' ps = do
+        let curve = bezier ps
+        setStrokeStyle "cyan" ctx
+        strokePath ctx $ line ctx $ curve <$> steps 100
+
+        let p = curve t
+        setFillStyle "black" ctx
+        fillPath ctx $ circle ctx { x: p.x, y: p.y, r: 3.0 }
+
+        maybeDo (tail' ps) \ps_tail -> do
+          let ps_init = head ps :| init ps_tail
+
+          when (length ps > 2) do
+            setStrokeStyle "orange" ctx
+            strokePath ctx $ line ctx [bezier ps_init t, bezier ps_tail t]
+
+          go' ps_init
+          go' ps_tail
+
+  go $ (\{ x, y } -> { x: x / 2.0, y }) <$> points
+  go' $ (\{ x, y } -> { x: (x + width) / 2.0, y }) <$> points
 
   requestAnimationFrame $ draw canvas
 
